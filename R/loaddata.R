5351c4bd3e812d3945958244ef12507c8e419ee9
Sedithesa <s.d.t.sassen@gmail.com>
2022-06-10 16:17:09 +0200

    Version that accepts csv and excel files and imports database

--- R/loaddata.R
+++ R/loaddata.R
@@ -1,17 +1,12 @@
-require(readxl)
 
 
-name <- 'Test_with_mistakes_small'
-file_type <- 'xlsx'
-
-
-test <- function(name,file_type){
+load.data <- function(name,file_type){
   if(tolower(file_type) == 'xlsx'){
     data <- paste0(name,'.', file_type)
     db <- read_excel(data)
   }else if(tolower(file_type) == 'csv'){
     data <- paste0(name,'.','csv')
-    db  <- data
+    db  <- read.csv(data)
   }else{
     db <- 'Unknown file type'
   }
