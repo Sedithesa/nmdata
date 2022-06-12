667ca08990ff3b5a7e3d6c3ca1e6c5a3e2628f1b
Sedithesa <s.d.t.sassen@gmail.com>
2022-06-12 10:18:04 +0200

    refer to function of external package explicitly

--- R/loaddata.R
+++ R/loaddata.R
@@ -5,7 +5,7 @@
 load.data <- function(name,file_type){
   if(tolower(file_type) == 'xlsx'){
     data <- paste0(name,'.', file_type)
-    db <- read_excel(data)
+    db <- readxl::read_excel(data)
   }else if(tolower(file_type) == 'csv'){
     data <- paste0(name,'.','csv')
     db  <- read.csv(data)
