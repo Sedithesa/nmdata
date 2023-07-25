e8e4b846d7ceab97f0ec33893df15ca4cf061f0c
Sedithesa <52563799+Sedithesa@users.noreply.github.com>
2023-07-25 11:23:54 +0200

    Added NA value as optional argument for loading data

--- R/loaddata.R
+++ R/loaddata.R
@@ -1,7 +1,7 @@
 
 #' function to import data file based on file type (either 'xlsx' or 'csv')
 #'
-#' @param name file name
+#' @param name file name without extension
 #' @param file_type Type of file xlsx or csv
 #' @keywords
 #' @export
@@ -9,13 +9,21 @@
 
 
 
-load.data <- function(name,file_type){
+load.data <- function(name,file_type,na_value){
   if(tolower(file_type) == 'xlsx'){
     data <- paste0(name,'.', file_type)
-    db <- readxl::read_excel(data)
+    if(missing(na_value)){
+      db <- readxl::read_excel(data)}
+    else{
+      db <- readxl::read_excel(data, na = na_value)
+    }
   }else if(tolower(file_type) == 'csv'){
     data <- paste0(name,'.','csv')
-    db  <- read.csv(data)
+    if(missing(na_value)){
+      db  <- read.csv(data)}
+    else{
+      db  <- read.csv(data, na.strings = na_value)
+    }
   }else{
     db <- 'Unknown file type'
   }
