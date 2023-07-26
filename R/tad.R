9fb781f719933716dbb11d432140f60250005d85
Sedithesa <52563799+Sedithesa@users.noreply.github.com>
2023-07-26 15:57:23 +0200

    R script added to calculate time after dose
    For now only if there is no ADDL in columns.
    Date and time columns read as strings and format is hard coded as "%m/%d/%Y %H:%M%S" maybe make this the default and accept other formats as arguments of function

--- R/tad.R
+++ R/tad.R
@@ -8,6 +8,40 @@
 #' @export
 #' @examples -
 
-tad <- function(date,time){
-  date+time
+
+# Renames date and time columns as DATE and TIME.
+# Takes character values of DATE and TIME columns in format specified
+# in the function. Then creates a date time (DTTM) column.
+
+tad <- function(database,date='DATE',time='TIME',dose='DOSE',id='ID'){
+  TAD <- c()
+  db = database
+  colnames(db)[colnames(db) == date] <- 'DATE'
+  colnames(db)[colnames(db) == time] <- 'TIME'
+  colnames(db)[colnames(db) == dose] <- 'DOSE'
+
+  db$DTTM <- as.POSIXct(paste(db$DATE, db$TIME), format="%m/%d/%Y %H:%M:%S")
+
+  # arrange per ID and DTTM
+  db <- db[order(db$ID,db$DTTM),]
+
+  ldose <- 0
+
+  # Without ADDL
+  if(!('ADDL' %in% colnames(db))){
+    for(i in 1:nrow(db)){
+      if(!is.na(db$DOSE[i])){
+        ldose = db$DTTM[i]
+        TAD <- c(TAD,0)
+      }else{
+        TAD <- c(TAD, difftime(db$DTTM[i],ldose, units = 'hours'))
+      }
+    }
+  }
+
+  db2 <- cbind(db,TAD)
+  db2$TAD <- as.numeric(db2$TAD)
+  return(db2)
 }
+
+
