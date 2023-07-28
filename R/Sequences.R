558c391d704e54ce638dd2e5bcf82eebf5e81afc
TPreijers <TPreijers@users.noreply.github.com>
2023-07-28 20:19:17 +0200

    Added sequences

---
+++ R/Sequences.R
@@ -0,0 +1,17 @@
+#' Create sequences for NONMEM dataset
+#'
+#' @param id_col Values from the (patient) ID column
+#' @param occ_col Values from the occasion (OCC) column
+#'
+#' @examples  seq_per_id(ds$ID, ds$OCC)
+
+
+seq_per_id <- function(id_col){
+  ave(1:length(id_col), id_col, FUN=function(x){1:length(x)})
+}
+
+seq_per_occ <- function(id_col, occ_col){
+  ave(1:length(id_col), id_col, occ_col, FUN=function(x){1:length(x)})
+}
+
+
