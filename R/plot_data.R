0fa29d43dda615a76e23647748d75eafaabd9491
TPreijers <TPreijers@users.noreply.github.com>
2023-07-28 20:06:44 +0200

    Changed the following: - added error handling - added ds filtering

--- R/plot_data.R
+++ R/plot_data.R
@@ -7,8 +7,22 @@
 #' @examples
 
 
-data_plot <- function(database, time = 'TAD', dv = 'DV'){
+plot_data <- function(database, time = 'TAD', dv = 'DV'){
+
+  # Error exceptions
+  if(!c(time %in% names(database))) {
+    stop(paste('No column in datatset with name:', time))
+  }
+  if(!c(dv %in% names(database))) {
+    stop(paste('No column in datatset with name:', dv))
+  }
+
+  # Database transformations
+  database <- database[database$MDV==0,]
+
+  # Plotting
   ggplot(database) +
-    geom_point(aes_string(time,dv))+ xlim(0,24) + theme_bw()
+    geom_point(aes_string(time,dv))+ xlim(0,24) +
+    theme_bw()
 }
 
