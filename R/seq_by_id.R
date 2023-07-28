b55fe495b8769d5b8e7230a631ce99be2a2c1d50
TPreijers <TPreijers@users.noreply.github.com>
2023-07-28 22:09:16 +0200

    d

--- R/seq_by_id.R
+++ R/seq_by_id.R
@@ -1,8 +1,12 @@
+
+#' Create sequences for NONMEM dataset
+#' @description
 #' Create sequences for NONMEM dataset
 #'
 #' @param id_col Values from the (patient) ID column
 #' @param occ_col Values from the occasion (OCC) column
 #' @export
+#' @keywords seq_per_id
 #' @examples  nmdataset$SEQ_IDS <- seq_per_id(ds$ID)
 #' @examples  nmdataset$SEQ_OCC <- seq_per_occ(ds$ID, ds$OCC)
 
@@ -10,4 +14,3 @@
 seq_per_id <- function(id_col){
   ave(1:length(id_col), id_col, FUN=function(x){1:length(x)})
 }
-
