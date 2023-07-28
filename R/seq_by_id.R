b3684b75db8b6a2d004ec8f86c831d76b54f2160
TPreijers <TPreijers@users.noreply.github.com>
2023-07-28 22:02:11 +0200

    hele handel

--- R/seq_by_id.R
+++ R/seq_by_id.R
@@ -2,7 +2,7 @@
 #'
 #' @param id_col Values from the (patient) ID column
 #' @param occ_col Values from the occasion (OCC) column
-#'
+#' @export
 #' @examples  nmdataset$SEQ_IDS <- seq_per_id(ds$ID)
 #' @examples  nmdataset$SEQ_OCC <- seq_per_occ(ds$ID, ds$OCC)
 
@@ -11,6 +11,3 @@ seq_per_id <- function(id_col){
   ave(1:length(id_col), id_col, FUN=function(x){1:length(x)})
 }
 
-seq_per_occ <- function(id_col, occ_col){
-  ave(1:length(id_col), id_col, occ_col, FUN=function(x){1:length(x)})
-}
