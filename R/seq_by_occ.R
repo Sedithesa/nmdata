b161e149592484cd2fefe7576ec6950f3f895dbf
TPreijers <TPreijers@users.noreply.github.com>
2023-07-28 22:16:26 +0200

    ssss

--- R/seq_by_occ.R
+++ R/seq_by_occ.R
@@ -6,11 +6,11 @@
 #' @param id_col Values from the (patient) ID column
 #' @param occ_col Values from the occasion (OCC) column
 #' @export
-#' @keywords seq_per_occ
+#' @keywords seq_by_occ
 #' @examples  nmdataset$SEQ_IDS <- seq_per_id(ds$ID)
 #' @examples  nmdataset$SEQ_OCC <- seq_per_occ(ds$ID, ds$OCC)
 
 
-seq_per_occ <- function(id_col, occ_col){
+seq_by_occ <- function(id_col, occ_col){
   ave(1:length(id_col), id_col, occ_col, FUN=function(x){1:length(x)})
 }
